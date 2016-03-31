e86426c0a707636e1b54390dc6e2c508cfa410a4
Justin Ordonez <MOTYSHIZ@gmail.com>
2016-03-30 18:58:51 -0700

    Added Video 4 Concepts: ColourMap

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class MapGenerator : MonoBehaviour {
 
+    public enum DrawMode {NoiseMap, ColourMap};
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -17,12 +20,40 @@ public class MapGenerator : MonoBehaviour {
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth,mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        //Detecting height and assigning colour.
+        Color[] colourMap = new Color[mapWidth*mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+        
     }
 
     //Set boundaries on input values in Inspector panel.
@@ -32,5 +63,14 @@ public class MapGenerator : MonoBehaviour {
         if (mapHeight < 1) mapHeight = 1;
         if (lacunarity < 1) lacunarity = 1;
         if (octaves < 0) octaves = 0;
+    }
+
+    // A TerrainType for each height color level.
+    [System.Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color colour;
     }
 }
